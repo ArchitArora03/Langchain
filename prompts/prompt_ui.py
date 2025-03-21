e225e24814df59d0cd302c400c3866490cb2270f
ArchitArora03 <archit.arora.sms@gmail.com>
2025-03-21 19:11:53 +0530

    create chain and storing template in a .json

--- prompts/prompt_ui.py
+++ prompts/prompt_ui.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import streamlit as st
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -15,30 +15,15 @@ st.header("Research Tool")
 paper_input= st.selectbox("Selected Research paper name", ["Attention is all you need", "BERT: Pre-training of Deep Bidirectional Transformers"])
 style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] )
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+template = load_prompt('template.json')
 
-template= PromptTemplate(
-    template="""
-Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}  
-Explanation Length: {length_input}  
-1. Mathematical Details:  
-   - Include relevant mathematical equations if present in the paper.  
-   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
-2. Analogies:  
-   - Use relatable analogies to simplify complex ideas.  
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
-Ensure the summary is clear, accurate, and aligned with the provided style and length.
-""",
-input_variables=['paper_input', 'style_input','length_input'],
-validate_template=True )
 
-prompt=template.invoke({
-    'paper_input':paper_input,
-    'style_input':style_input,
-    'length_input':length_input
 
-})
-
-if st.button ('Summarize'):
-    result=model.invoke(prompt)
+if st.button('Summarize'):
+    chain = template | model
+    result = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
     st.write(result.content)
