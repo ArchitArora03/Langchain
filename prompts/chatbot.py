9424f923735b23c4a913044139c550c0b2d5c50c
ArchitArora03 <archit.arora.sms@gmail.com>
2025-03-24 12:25:06 +0530

    Adding chat history to chatbot

--- prompts/chatbot.py
+++ prompts/chatbot.py
@@ -5,11 +5,16 @@ load_dotenv()
 llm= HuggingFaceEndpoint(repo_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',task='text-generation')
 model=ChatHuggingFace(llm=llm)
 
+chat_history=[]
 while True:
     user_input=input('You: ')
+    chat_history.append(user_input)
     if user_input=='Exit':
         break
     else:
-        result=model.invoke(user_input)
+        result=model.invoke(chat_history)
+        chat_history.append(result.content)
         print('AI:',result.content)
+
+print(chat_history)
     
